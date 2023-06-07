74515c5a4304847f8eecacebac7086851f7ea7a6
Christina <christina.phillips9838@gmail.com>
2023-06-07 19:10:23 -0400

    Added pagination to the search results screen

--- client/src/Components/pages/Search.js
+++ client/src/Components/pages/Search.js
@@ -1,8 +1,8 @@
-import React, {useEffect, useState}from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../App.css';
 import { useSelector, useDispatch } from 'react-redux';
 import './Search.css';
-import {isEmpty} from "lodash";
+import { isEmpty } from "lodash";
 import FavoriteButton from '../FavoriteButton';
 import { Link } from 'react-router-dom';
 
@@ -12,9 +12,12 @@ function Search() {
   const toInsert = useSelector((state) => (state.favoriting.value.toInsert));
   const toRemove = useSelector((state) => (state.favoriting.value.toRemove));
 
+  const [paginate, setpaginate] = useState(10);
+  const [loadMore, setLoadMore] = useState(false);
+
   const [backendData, setBackendData] = useState([{}])
   useEffect(() => {
-    async function fetchData (){
+    async function fetchData() {
       console.log("calling fetch data..");
       const response = await fetch(`http://localhost:5000/search/${searchType.searchType}/${searchWord.searchWord}`);
       const data = await response.json();
@@ -24,59 +27,74 @@ function Search() {
     fetchData();
   }, []);
 
+
+  const load_more = (event) => {
+    setpaginate((prevValue) => prevValue + 10);
+    if(paginate >= 40){
+      setLoadMore(false);
+    }
+  };
+
   return (
     <span className="font-link">
       {
         (isEmpty(backendData)) ? (
           <p className='loading'>No results found</p>
-        ):(
+        ) : (
           <div className='search-Search'>
             {(typeof backendData[0].title === 'undefined') ? (
-            <p className="loading">Loading...</p>  
-            ): (
-              backendData.map((book, key) =>{
-              let authorString = "";
-          
-              let title = book.title ? book.title : "No title";
-              let authors = book.author ? book.author : ["No author"];
-              let description = book.description ? book.description : "No description available";
-              let image = book.img ? book.img : "https://www.freeiconspng.com/uploads/no-image-icon-10.png";
+              <p className="loading">Loading...</p>
+            ) : (
+              backendData.slice(0, paginate)
+                .map((book, key) => {
+                  let authorString = "";
 
-              authors.forEach((author) => {
-                author += ", ";
-                authorString += author;
-              })
-          
-              authorString = authorString.slice(0, authorString.length - 2);
+                  let title = book.title ? book.title : "No title";
+                  let authors = book.author ? book.author : ["No author"];
+                  let description = book.description ? book.description : "No description available";
+                  let image = book.img ? book.img : "https://www.freeiconspng.com/uploads/no-image-icon-10.png";
 
-              if(description.length > 600){
-                description = description.slice(0, 600);
-                description += "...";
-              }
+                  authors.forEach((author) => {
+                    author += ", ";
+                    authorString += author;
+                  })
 
-              return(
-                <div key={key}>
-                <div className='search-imgDescription'>
-                  <Link to='/product' state={{props: book}}>
-                    <img src = {image} width="128" height="192"/>
-                  </Link>
-                  
-                  <FavoriteButton
-                  props={book}/>
-                  <p className="search-description">{description}</p>
-                </div>
-                <div className='search-words'>
-                  <h3 className='search-title'>{title}</h3>
-                  <p className='search-author'>{authorString}</p>
-                </div>
-                </div>
-              )
-            })
-          )}
-        </div>
+                  authorString = authorString.slice(0, authorString.length - 2);
+
+                  if (description.length > 600) {
+                    description = description.slice(0, 600);
+                    description += "...";
+                  }
+
+                  return (
+                    <div key={key}>
+                      <div className='search-imgDescription'>
+                        <Link to='/product' state={{ props: book }}>
+                          <img src={image} width="128" height="192" />
+                        </Link>
+
+                        <FavoriteButton
+                          props={book} />
+                        <p className="search-description">{description}</p>
+                      </div>
+                      <div className='search-words'>
+                        <h3 className='search-title'>{title}</h3>
+                        <p className='search-author'>{authorString}</p>
+                      </div>
+                    </div>
+                  )
+                })
+            )}
+            <div className='load-more'>
+              {(typeof backendData[0].title != 'undefined' && paginate < 40)? 
+                (<button onClick={load_more}>Load More</button>): 
+                (<span></span>)}
+            </div>
+            
+          </div>
         )
       }
-    
+
     </span>
   )
 }
